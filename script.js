cb04ed4e7a108048537d1e1db0cdd378b1c70ff3
Radwan <tara02664@gmail.com>
2023-07-06 20:12:15 +0300

    (ATR)

---
+++ script.js
@@ -0,0 +1,23 @@
+(function(){
+    const form = document.querySelector('#message-form')
+    
+    form.addEventListener('submit',function(e){
+        e.preventDefault()
+        const message = document.querySelector('#message')
+        const feedback = document.querySelector('.feedback')
+        const messagecontent = document.querySelector('.message-content')
+
+        if(message.value === ''){
+                feedback.classList.add('show')
+            setTimeout(function(){
+                feedback.classList.remove('show')
+            },4000)
+        }else{
+            messagecontent.textContent = message.value
+            message.value = ''
+        }
+
+    })
+
+})()
+
